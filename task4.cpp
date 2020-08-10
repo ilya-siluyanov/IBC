c6883d772159180ee57399ce51580cd0c3b1cf4a
Ilya Siluyanov <isiluyanov8@gmail.com>
2020-08-10 17:58:22 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -3,16 +3,81 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <map>
 
 using namespace std;
 #define P pair<double,double>
 #define pb push_back
 typedef long long ll;
+long long T = 1000000;
 
 bool cmpy(P &a, P &b) {
     return a.second < b.second;
 }
 
+double len(P a) {
+    return sqrt(pow(a.first, 2) + pow(a.second, 2));
+}
+
+double len(P a, P b) {
+    return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
+double cosMult(P a) {
+    P b = {T, 0};
+    double first = a.first * b.first + a.second * b.second;
+    double second = len(a) * len(b);
+    return first / second;
+}
+
+
+double sinMult(P a) {
+    P b = {T, 0};
+
+    double first = b.first * a.second - b.second * a.first;
+    double second = len(a) * len(b);
+    return first / second;
+}
+
+double angle(double cos, double sin) {
+    double angle = acos(cos);
+    if (sin < 0) {
+        angle = 2 * M_PI - angle;
+    }
+    return angle;
+}
+
+struct node {
+    double x;
+    double y;
+    double angle;
+
+    node(double x, double y, double angle) : x(x), y(y), angle(angle) {}
+};
+
+bool cmpAngle(node a, node b) {
+    return a.angle < b.angle;
+}
+
+
+double roun(double angle) {
+    double ang = angle * T;
+    double an = round(ang);
+    an = an / (1.0 * T);
+    return an;
+}
+
+P direct(P a, P b) {
+    double ax = (a.second - b.second);
+    double bx = (a.first - b.first);
+    double k = (ax) / (bx);
+    double bc = a.second - k * a.first;
+    if (isnan(bc) || isnan(k) || isinf(k) || isinf(-k)) {
+        k = INFINITY;
+        bc = a.first;
+    }
+    return {k, bc};
+}
 
 int main() {
     double x1, y1, x2, y2, x3, y3, x4, y4, x5, y5;
@@ -22,56 +87,103 @@ int main() {
     dots.pb({x2, y2});
     dots.pb({x3, y3});
     dots.pb({x4, y4});
-    set<double> xs = {x1, x2, x3, x4};
-    if (xs.size() == 2) {
-        for (int i = 0; i < 4; i++)
-            swap(dots[0].first, dots[0].second);
+    set<P > dirs;
+    set<P > was;
+    for (int i = 0; i < 4; i++) {
+        vector<node> v;
+        for (int j = 0; j < 4; j++) {
+            if (i == j) continue;
+
+            P dot = {dots[j].first - dots[i].first, dots[j].second - dots[i].second};
+            double cos = roun(cosMult(dot));
+            double sin = roun(sinMult(dot));
+            double angl = angle(cos, sin);
+            double deg = 1.0 * 180 / M_PI * angl;
+            v.push_back(node(dots[j].first, dots[j].second, angl));
+            was.insert({min(i, j), max(i, j)});
+        }
+        sort(v.begin(), v.end(), cmpAngle);
+
+        dirs.insert(direct({v[0].x, v[0].y}, dots[i]));
+        dirs.insert(direct({v[2].x, v[2].y}, dots[i]));
     }
-    sort(dots.begin(), dots.end(), cmpy);
-    for (int i = 0; i < 4; i += 2) {
-        if (dots[i].first > dots[i + 1].first) {
-            P t = dots[i];
-            dots[i] = dots[i + 1];
-            dots[i + 1] = t;
+    map<double, int> cnt;
+    for (P d:dirs) {
+        cnt[d.first]++;
+    }
+    set<P > rdirs;
+    for (P x:cnt) {
+        if (x.second > 1) {
+            for (P d:dirs) {
+                if (d.first == x.first)
+                    rdirs.insert(d);
+            }
         }
     }
-    P d1 = dots[0];
-    P d2 = dots[2];
-    P d3 = dots[1];
-    P d4 = dots[3];
-    x1 = d1.first;
-    x2 = d2.first;
-    x3 = d3.first;
-    x4 = d4.first;
-    y1 = d1.second;
-    y2 = d2.second;
-    y3 = d3.second;
-    y4 = d4.second;
-    //line is {k,b}, y = kx+b
-    double k = (y2 - y1) / (x2 - x1);
-    P firstLine = {k, y1 - k * x1};
-    k = (y4 - y2) / (x4 - x2);
-    P secondLine = {k, y2 - k * x2};
-    k = (y4 - y3) / (x4 - x3);
-    P thirdLine = {k, y3 - k * x3};
-    k = (y3 - y1) / (x3 - x1);
-    P fourthLine = {k, y1 - k * x1};
-    if (firstLine.second < thirdLine.second) {
-        swap(firstLine, thirdLine);
+    dirs = rdirs;
+    for (P x:dirs) {
+        if (x.first == -0) {
+            dirs.erase(x);
+            x.first = 0;
+            dirs.insert(x);
+        }
     }
+    cnt = map<double, int>();
+    vector<P > pr;
+    for (P x:dirs) {
+        cnt[x.first]++;
+        pr.pb(x);
+    }
+    vector<P > first;
+    vector<P > second;
+    double f = cnt.begin()->first;
+    double s = cnt.rbegin()->first;
+    for (P y:pr) {
+        if (y.first == f) {
+            first.pb(y);
+        }
+    }
+    for (P y:pr) {
+        if (y.first == s) {
+            second.pb(y);
+        }
+    }
+    if (first[0].second > first[1].second)swap(first[0], first[1]);
+    if (second[0].second > second[1].second)swap(second[0], second[1]);
+
     int ok = 0;
-    if (firstLine.first * x5 + firstLine.second >= y5) {
-        if (secondLine.first * x5 + secondLine.second >= y5) {
-            if (thirdLine.first * x5 + thirdLine.second <= y5) {
-                if (fourthLine.first * x5 + fourthLine.second <= y5) {
+    bool one = first[0].first * x5 + first[0].second <= y5;
+    if (isinf(first[0].first)) {
+        one = false;
+        one = first[0].second <= x5;
+    }
+    bool two = first[1].first * x5 + first[1].second >= y5;
+    if (isinf(first[1].first)) {
+        two = false;
+        two = first[1].second >= x5;
+    }
+    bool three = second[0].first * x5 + second[0].second <= y5;
+    if (isinf(second[0].first)) {
+        three = false;
+        three = second[0].second <= x5;
+    }
+    bool four = second[1].first * x5 + second[1].second >= y5;
+    if (isinf(second[1].first)) {
+        four = false;
+        four = second[1].second >= x5;
+    }
+    if (one) {
+        if (two) {
+            if (three) {
+                if (four) {
                     ok = 1;
                 }
             }
         }
     }
     if (ok) {
-        cout << "YES";
+        cout << "YES" << endl;
     } else {
-        cout << "NO";
+        cout << "NO" << endl;
     }
 }
