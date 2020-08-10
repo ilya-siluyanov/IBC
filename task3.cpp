dfd5da95b824bef12d8627f69101fd58d07aa4e6
Ilya Siluyanov <isiluyanov8@gmail.com>
2020-08-10 14:09:44 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b) {
+    if (a == 0)return b;
+    if (b == 0)return a;
+    if (a >= b)return gcd(b, a % b);
+    return gcd(a, b % a);
+}
+
+int main() {
+    ll a, b;
+    cin >> a >> b;
+    cout << gcd(a, b);
+}
